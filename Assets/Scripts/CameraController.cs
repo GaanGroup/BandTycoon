255d3f9d28b41d7e96b46c583538788c9a13400c
nyxad <30995260+nyxad@users.noreply.github.com>
2018-10-10 20:27:45 -0400

    added to camera script, added room model

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+    private Transform tr;
+    private Transform cameraTr;
+
 	// Use this for initialization
 	void Start () {
-		
+        tr = transform;
+        cameraTr = Camera.main.transform;
 	}
 	
 	// Update is called once per frame
